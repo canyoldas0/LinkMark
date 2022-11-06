bbdbc945575c9f7775a0b910067e8041d484daf6
Can Yoldas <can.yoldas0@outlook.com>
2022-11-06 23:24:21 +0300

    Initial Commit

---
+++ TrackLink/TrackLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackLinkApp.swift
+//  TrackLink
+//
+//  Created by Can Yoldaş on 6.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
